730f88f1d4a920d09e33593452578d2cf994c8e4
josephstorey <jsto890@github.com>
2023-03-13 13:36:25 +1300

    New Docstrings for code

--- functions_ttt.py
+++ functions_ttt.py
@@ -5,7 +5,8 @@ def initialise_board():
         Returns:
         board (list): A list of 9 strings, each representing an empty cell on the board.
     """
-    board = ('.', '.', '.', '.', '.', '.', '.', '.', '.')  # Creates the list that forms the board
+    board = ('.', '.', '.', '.', '.', '.', '.', '.', '.')
+    # Creates the list that forms the board
     board = list(board)
     return board
 
@@ -17,14 +18,18 @@ def display_board(board):
         Args:
         board (list): A list of 9 strings, each representing a cell on the board.
     """
-    print(board[0] + '|' + board[1] + '|' + board[2])  # First line of board
+    print(board[0] + '|' + board[1] + '|' + board[2])
+    # First line of board
     print('-+-+-')
-    print(board[3] + '|' + board[4] + '|' + board[5])  # Second line of board
+    print(board[3] + '|' + board[4] + '|' + board[5])
+    # Second line of board
     print('-+-+-')
-    print(board[6] + '|' + board[7] + '|' + board[8])  # Third line of board
+    print(board[6] + '|' + board[7] + '|' + board[8])
+    # Third line of board
 
 
-def get_current_turn_number(board):  # Counts the number of turns
+def get_current_turn_number(board):
+    # Counts the number of turns
     """
         Calculates the current turn number based on the number of moves made on the board.
 
@@ -36,15 +41,18 @@ def get_current_turn_number(board):  # Counts the number of turns
     """
     count = 1
     i = 0
-    while i < 9:  # Repeats through 0 to 8, all our empty cells
-        if board[i] != '.':  # Finds cells that are full and counts them
+    while i < 9:
+        # Repeats through 0 to 8, all our empty cells
+        if board[i] != '.':
+            # Finds cells that are full and counts them
             count += 1
         i += 1
 
     return count
 
 
-def get_current_player(board):  # Finds the current player that has action on him
+def get_current_player(board):
+    # Finds the current player that has action on him
     """
         Determines which player's turn it is based on the current state of the board.
 
@@ -58,15 +66,19 @@ def get_current_player(board):  # Finds the current player that has action on hi
     countO = 0
     i = 0
     while i < 9:
-        if board[i] == 'X':  # Counts the X plays in progress
+        if board[i] == 'X':
+            # Counts the X plays in progress
             countX += 1
-        if board[i] == 'O':  # Counts the O plays in progress
+        if board[i] == 'O':
+            # Counts the O plays in progress
             countO += 1
         i += 1
-    if countX > countO:  # Compares them and if more X plays then it is player O's turn
+    if countX > countO:
+        # Compares them and if more X plays then it is player O's turn
         player = 'Player O'
 
-    else:  # Other option
+    else:
+        # Other option
         player = 'Player X'
     return player
 
@@ -85,24 +97,33 @@ def play_turn(board, a, b):
         Returns:
         board (list): the updated board state after the player's move
     """
-    if 1 <= (a and b) <= 3:  # Values within the range permitted
-        c = (3 * (a - 1)) + (b - 1)  # Correlating coordinates to board number
+    if 1 <= (a and b) <= 3:
+        # Values within the range permitted
+        c = (3 * (a - 1)) + (b - 1)
+        # Correlating coordinates to board number
 
-        count = get_current_turn_number(board)  # Finding out turns easy within the function
-        if count % 2 == 1:  # Odd = X
-            if board[c] == '.':  # If board is 'empty' player can place their piece
+        count = get_current_turn_number(board)
+        # Finding out turns easy within the function
+        if count % 2 == 1:
+            # Odd = X
+            if board[c] == '.':
+                # If board is 'empty' player can place their piece
                 board[c] = 'X'
 
             else:
-                board[c] = 'O' or 'X'  # If not then error is printed
+                board[c] = 'O' or 'X'
+                # If not then error is printed
                 print('This move is not available')
 
-        elif count % 2 == 0:  # Even  = O
-            if board[c] == '.':  # If board is 'empty' player can place their piece
+        elif count % 2 == 0:
+            # Even  = O
+            if board[c] == '.':
+                # If board is 'empty' player can place their piece
                 board[c] = 'O'
 
             else:
-                board[c] = 'X' or 'O'  # If not then error is printed
+                board[c] = 'X' or 'O'
+                # If not then error is printed
                 print('This move is not available')
 
     else:
@@ -119,10 +140,12 @@ def check_draw(board):
         Returns:
         False: If the board is not a draw
     """
-    count = get_current_turn_number(board)  # Brings in count to function
+    count = get_current_turn_number(board)
+    # Brings in count to function
     if count == 10:
         display_board(board)
-        print('\nGAME OVER\n')  # If count reaches 8 then game has tied
+        print('\nGAME OVER\n')
+        # If count reaches 8 then game has tied
         print('PLAYERS HAVE TIED')
         exit()
     else:
@@ -142,66 +165,79 @@ def check_win(board):
     If no win condition is found, it returns False.
     """
 
-    count = get_current_turn_number(board)  # Finding out turns easy within the function
+    count = get_current_turn_number(board)
+    # Finding out turns easy within the function
     countX = 0
     countO = 0
     i = 0
     while i < 9:
-        if board[i] == 'X':  # Counts the X plays in progress
+        if board[i] == 'X':
+            # Counts the X plays in progress
             countX += 1
-        if board[i] == 'O':  # Counts the O plays in progress
+        if board[i] == 'O':
+            # Counts the O plays in progress
             countO += 1
         i += 1
-    if countX > countO:  # Compares them and if more X plays then it is player O's turn
+    if countX > countO:
+        # Compares them and if more X plays then it is player O's turn
         player = 'Player X'
 
-    else:  # Other option
+    else:
+        # Other option
         player = 'Player O'
 
     while count >= 5:
-        if board[0] == board[1] == board[2] != '.':  # Along top
+        if board[0] == board[1] == board[2] != '.':
+            # Along top
             display_board(board)
             print('Game has ended')
             print('Well Played ' + player)
             exit()
 
-        elif board[3] == board[4] == board[5] != '.':  # Along middle
+        elif board[3] == board[4] == board[5] != '.':
+            # Along middle
             display_board(board)
             print('Game has ended')
             print('Well Played ' + player)
             exit()
 
-        elif board[6] == board[7] == board[8] != '.':  # Along bottom
+        elif board[6] == board[7] == board[8] != '.':
+            # Along bottom
             display_board(board)
             print('Game has ended')
             print('Well Played ' + player)
             exit()
 
-        elif board[0] == board[3] == board[6] != '.':  # Along 1st column
+        elif board[0] == board[3] == board[6] != '.':
+            # Along 1st column
             display_board(board)
             print('Game has ended')
             print('Well Played ' + player)
             exit()
 
-        elif board[1] == board[4] == board[7] != '.':  # Along 2nd column
+        elif board[1] == board[4] == board[7] != '.':
+            # Along 2nd column
             display_board(board)
             print('Game has ended')
             print('Well Played ' + player)
             exit()
 
-        elif board[3] == board[5] == board[8] != '.':  # Along 3rd column
+        elif board[3] == board[5] == board[8] != '.':
+            # Along 3rd column
             display_board(board)
             print('Game has ended')
             print('Well Played ' + player)
             exit()
 
-        elif board[0] == board[4] == board[8] != '.':  # Along L->R diagonal
+        elif board[0] == board[4] == board[8] != '.':
+            # Along L->R diagonal
             display_board(board)
             print('Game has ended')
             print('Well Played ' + player)
             exit()
 
-        elif board[3] == board[4] == board[6] != '.':  # Along R->L diagonal
+        elif board[3] == board[4] == board[6] != '.':
+            # Along R->L diagonal
             display_board(board)
             print('Game has ended')
             print('Well Played' + player)
